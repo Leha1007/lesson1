ce808346cba4b586a0baa60dfbb50c04205c3710
Leha1007 <alukasenko08@gmail.com>
2024-07-22 21:04:53 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,7 @@
+immutable_var = 1, 3.2, 'fjk', True
+print(immutable_var)
+#immutable_var[0] = 4
+#'tuple' object does not support item assignment
+mutable_list = [2, 34, 'kgl', False, 46.21]
+mutable_list[0] = True
+print(*mutable_list)
